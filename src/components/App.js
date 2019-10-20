d29f9b9eeb35a51ad116ec4a9c3181a496daa658
skillup <myEmail@example.com>
2019-10-20 15:48:02 +0300

    test

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import Footer from './Footer';
-import Header from './Header';
+import React,{ useState } from 'react';
+//import Footer from './Footer';
+//import Header from './Header';
+
+const names = ['vasa', 'pitya', 'alex', 'an'];
+
+const dateStart = new Date();
+
+const render = names.reduce((ak, name, i) => {
+  if (name !== 'pitya') {
+    ak.push(<p key={i}>{name}</p>)
+    return ak;
+  }
+  return ak;
+}, []);
+const dateResult = new Date() - dateStart;
+console.log(dateResult);
+
 function App() {
+  const [res, setRes]= useState('Hello')
+  const show = () => {
+    setRes(res + ' world');
+  }
   return (
-    <>
-      <Header />
-      <h2>Content</h2>
-      <Footer />
-    </>
+    <div id="app">
+      <h2 onClick={show}>Hello app</h2>
+      {res}
+    </div>
   );
 }
 
