cf68b79e2db9fc256ada53f5c9669d71540f495f
skillup <myEmail@example.com>
2019-10-20 13:26:44 +0300

    start project

---
+++ src/components/Header.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header>
+            <ul>
+                <li>Home</li>
+                <li>About as</li>
+                <li>Contacts</li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header;
